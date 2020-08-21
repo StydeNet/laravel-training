aacafb0ba504ea9a38264b2e4c1e1ab44ac1a3d5
Duilio Palacios <duilio@styde.net>
2020-08-21 12:27:26 +0100

    Add additional example

---
+++ database/migrations/2020_08_21_111900_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('title');
+            $table->mediumText('content');
+
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Post;
+use App\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * @test
+     * @testdox Un post pertenece a un autor
+     */
+    function a_post_belongs_to_an_author()
+    {
+        $this->markTestIncomplete();
+
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->create([
+            'author_id' => $user->id,
+        ]);
+
+        $this->assertInstanceOf(BelongsTo::class, $post->author());
+        $this->assertInstanceOf(User::class, $post->author);
+        $this->assertTrue($post->author->is($user));
+    }
+}
